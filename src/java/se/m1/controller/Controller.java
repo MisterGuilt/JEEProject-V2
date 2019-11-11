7a538de0e5534e270496042d9f932432677885a8
MisterGuilt <Bedebao@DESKTOP-91TM8MH>
2019-11-11 20:56:54 +0100

    Merge origin/master
    Conflicts:
    	src/java/Control/Controller.java

--- src/java/se/m1/controller/Controller.java
+++ src/java/se/m1/controller/Controller.java
@@ -1,7 +1,6 @@
 package se.m1.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
