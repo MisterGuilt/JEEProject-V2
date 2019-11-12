b324c50b9ff0adb2825635a5adf80847e824050f
MisterGuilt <37652522+MisterGuilt@users.noreply.github.com>
2019-11-12 11:43:47 +0100

    Merge origin/master
    Conflicts:
    	README.md

--- src/main/java/se/m1/ctrl/Controller.java
+++ src/main/java/se/m1/ctrl/Controller.java
@@ -28,6 +28,7 @@ public class Controller extends HttpServlet {
     
     @EJB
     private UserSB userSB;
+    ArrayList<User> listOfUsers;
     @EJB
     private EmployeesSB employeeSB;
     ArrayList<Employees> listOfEmployees;
@@ -40,6 +41,9 @@ public class Controller extends HttpServlet {
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        listOfUsers = new ArrayList<>();
+        listOfUsers.addAll(userSB.getUsers());
+
         currentSession = request.getSession(false);
         
         //If the user tries to log in
@@ -174,7 +178,7 @@ public class Controller extends HttpServlet {
                 request.getRequestDispatcher(JSP_GOODBYE_PAGE).forward(request, response);
             }
         }
-        //If nothing matches, redirect to the login page
+        //Just in case some freak accident happens, redirect to the login page
         else
         {
             request.getRequestDispatcher(JSP_HOME_PAGE).forward(request, response);
